16f1da68db44dbbaebf8129658158e919d1cf02a
ThereIsNotAWay <162099389+ThereIsNotAWay@users.noreply.github.com>
2024-04-08 11:27:51 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/*const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting)
+      {
+          entry.target.classList.add('show');
+      }
+      else 
+      {
+          entry.target.classList.remove('show');
+      }
+  });
+}, {
+  rootMargin: "-55px",
+});
+
+const hiddenElementsLeft = document.querySelectorAll('.hidden-left');
+const hiddenElementsRight = document.querySelectorAll('.hidden-right');
+hiddenElementsLeft.forEach((el) => observer.observe(el));
+hiddenElementsRight.forEach((el) => observer.observe(el)); */
