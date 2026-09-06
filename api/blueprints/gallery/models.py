923f6e5ee98fc0f0e48a9e496ea95e0c93e1fdfd
ThereIsNotAWay <unitywillprevail@gmail.com>
2026-09-06 16:09:41 -0400

    Updated routes.py and models.py to add functionality to sign, add, and delete images from gallery via upload while also validating those images.

--- api/blueprints/gallery/models.py
+++ api/blueprints/gallery/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, String, text, TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
+from pydantic import BaseModel, Field, HttpUrl, field_validator
+from enum import Enum
 
 from api.database import Base
 
@@ -7,6 +9,8 @@ class GalleryItem(Base):
     __tablename__ = "gallery"
 
     id = Column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"))
+    public_id = Column(String, nullable=False, unique=True)
+    # season = Column(String, nullable=False)
     category = Column(String, nullable=False)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
@@ -17,8 +21,73 @@ class GalleryItem(Base):
     def to_dict(self):
         return {
             "id": str(self.id),
+            "public_id": self.public_id,
             "category": self.category,
             "title": self.title,
             "description": self.description,
             "src": self.src
         }
+
+class ImageCategory(str, Enum):
+    FTC = "FTC"
+    FRC = "FRC"
+    SEAGLIDE = "SeaGlide"
+
+class ImageUpload(BaseModel):
+    public_id: str
+    category: ImageCategory
+    title: str
+    description: str | None = None
+    src: HttpUrl
+
+    @field_validator("public_id")
+    @classmethod
+    def validate_public_id(cls, passed_public_id: str) -> str:
+        passed_public_id = passed_public_id.strip()
+        if not passed_public_id:
+            raise ValueError("public_id cannot be empty!")
+
+        if len(passed_public_id) > 255:
+            raise ValueError("public_id is too long!")
+
+        return passed_public_id
+
+    @field_validator("title")
+    @classmethod
+    def validate_title(cls, passed_title: str) -> str:
+        passed_title = passed_title.strip()
+        if not passed_title:
+            raise ValueError("title cannot be empty!")
+
+        if len(passed_title) > 150:
+            raise ValueError("title must be 150 characters or less!")
+        
+        return passed_title
+
+    @field_validator("description")
+    @classmethod
+    def validate_description(cls, passed_description: str | None) -> str | None:
+        if passed_description is None:
+            return passed_description
+
+        if len(passed_description) == 0:
+            return passed_description
+
+        passed_description = passed_description.strip()
+
+        if not passed_description:
+            return None
+
+        if len(passed_description) > 150:
+            raise ValueError("description must be 150 characters or less!")
+
+        return passed_description
+
+    @field_validator("src")
+    @classmethod
+    def validate_src(cls, passed_src: HttpUrl) -> HttpUrl:
+        if "res.cloudinary.com" not in str(passed_src):
+            raise ValueError("src must be a Cloudinary URL!")
+
+        return passed_src
+    
--- api/blueprints/gallery/routes.py
+++ api/blueprints/gallery/routes.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, request, jsonify
+from flask_login import login_required
 from api.database import SessionLocal
-from api.blueprints.gallery.models import GalleryItem
+from api.blueprints.gallery.models import GalleryItem, ImageUpload
 
 gallery = Blueprint('gallery', __name__)
 
 GALLERY_DIR = "static/gallery"
 
-@gallery.get("/")
+@gallery.get("")
 def get_gallery():
     session = SessionLocal()
     try:
@@ -27,3 +28,51 @@ def get_gallery():
         return jsonify({str(e)}), 500
     finally:
         session.close()
+
+@gallery.route('/sign-image', methods=["POST"])
+@login_required
+def sign_image_signature():
+    unsigned_params = request.get_json()
+    signed_signature = cloudinary.utils.api_sign_request(unsigned_params, cloudinary.config().api_secret)
+    return jsonify({"signature": signed_signature})
+    
+@gallery.route('/images/del/<public_id>/', methods=["DELETE"])
+@login_required
+def delete_image(public_id):
+    session = SessionLocal()
+    cloudinary_response = cloudinary.uploader.destroy(public_id)
+
+    if cloudinary_response.get('result') != "ok":
+        return jsonify({"success": False, "message": cloudinary_response }), 400
+
+    image_to_be_deleted = session.scalar(select(GalleryItem).where(GalleryItem.public_id==public_id))
+    session.delete(image_to_be_deleted)
+    session.commit()
+    return jsonify({"success": True, "message": "Image successfully deleted"}), 200
+
+@gallery.route('/images/add', methods=["POST"])
+def add_image():
+  try: 
+    if not request.is_json:
+      return jsonify({"success": False, "message": "Invalid JSON"}), 415
+
+    upload_form = request.get_json()
+    upload_attempt = ImageUpload.model_validate(upload_form)
+
+    with SessionLocal() as session:
+      public_id = upload_attempt.public_id
+      category = upload_attempt.category
+      title = upload_attempt.title
+      description = upload_attempt.description
+      src = upload_attempt.src
+    
+      image = GalleryImage(public_id=public_id, category=category, title=title, description=description, src=src)
+
+      session.add(image)
+      session.commit()
+
+  except ValidationError as e:
+    return jsonify({"success": False, "message": "Invalid credentials!"}), 400
+
+  except BadRequest:
+    return jsonify({"success": False, "message": "Malformed JSON"}), 400
